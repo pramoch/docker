7d44ecae8d83915363416dda09f40c0f7aca0876
Pramoch Chulapong <pramoch.chulapong@gmail.com>
2020-08-07 15:40:01 +0700

    Update date/time format

--- node-app/index.js
+++ node-app/index.js
@@ -8,7 +8,7 @@ app.use(cors())
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.get('/time', (req, res) => {
-  res.send((new Date()).toUTCString())
+  res.send((new Date()).toString())
 })
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
